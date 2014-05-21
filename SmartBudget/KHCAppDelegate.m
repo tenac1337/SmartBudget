3b7eb0d29694d969a7848914d61e854f4aa339d7
kevinchon <compchon@gmail.com>
2014-05-20 22:51:03 -0400

    bullshit3

--- SmartBudget/KHCAppDelegate.m
+++ SmartBudget/KHCAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    NSLog(@"I made a change");
     return YES;
 }
 							
